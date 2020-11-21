04aad1c72f41f3e7e9f5bb49c39491ebfdd201b6
Colin Fendrick <colin.fendrick@gmail.com>
2020-11-20 18:29:14 -0800

    Remove pycache and fix a bug

--- backend/django_app/prediction/urls.py
+++ backend/django_app/prediction/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-import prediction.views as views
+from . import views
 
 urlpatterns = [
     path('/predict', views.IRIS_MODEL_PREDICT.as_view(), name='api_predict')
--- backend/django_app/prediction/views.py
+++ backend/django_app/prediction/views.py
@@ -2,11 +2,13 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from prediction.apps import PredictionConfig
+from . import apps
 import pandas as pd
 
 # Class based view to predict based on IRIS model
-class IRIS_Model_Predict(APIView):
+
+
+class IRIS_MODEL_PREDICT(APIView):
     def post(self, request, format=None):
         data = request.data
         keys = []
@@ -15,7 +17,7 @@ class IRIS_Model_Predict(APIView):
             keys.append(key)
             values.append(data[key])
         X = pd.Series(values).to_numpy().reshape(1, -1)
-        loaded_mlmodel = PredictionConfig.mlmodel
+        loaded_mlmodel = apps.PredictionConfig.mlmodel
         y_pred = loaded_mlmodel.predict(X)
         y_pred = pd.Series(y_pred)
         target_map = {0: 'setosa', 1: 'versicolor', 2: 'virginica'}
